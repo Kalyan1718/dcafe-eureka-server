9f821ba1c2a755b9c7b38e1acf52043ad7ab23cb
Kalyan1718 <durgakalyan018@gmail.com>
2024-08-21 12:40:11 +0530

    First commit

---
+++ src/main/java/in/dcafe/eureka/DcafeEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package in.dcafe.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DcafeEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcafeEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/dcafe/eureka/DcafeEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.dcafe.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DcafeEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
